0e0551055157b49fb8125b0015be43b2c9ad5f3d
Sam Townsend <sft3hy@virginia.edu>
2024-09-02 11:33:49 -0700

    website updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m $1
+git commit -m 'website updates'
 git push
 npm run build
 git add dist -f
--- personal_site/src/main.js
+++ personal_site/src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createMetaManager } from 'vue-meta';
 import App from './App.vue'
 import router from './router'
 
