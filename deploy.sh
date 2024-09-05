39f7b9f6ce61cef77b67c936be37e3be94a79d96
Sam Townsend <sft3hy@virginia.edu>
2024-09-05 13:43:38 -0700

    website updates

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-npm run build
-git add .
-git commit -m 'website updates'
-git push
-git add dist -f
-git commit -m 'redeploying'
-git subtree push --prefix dist origin gh-pages
--- src/utils/common.js
+++ src/utils/common.js
@@ -6,7 +6,7 @@ export function createTag(type, propertyName, property, content) {
     tag.content = content;
     document.head.insertBefore(tag, document.head.firstChild);
 }
-const toRemove = 'meta[name="description"], meta[property="og:title"], meta[property="og:site_name"], meta[property="og:description"]'
+const toRemove = 'meta[name="description"], meta[property="og:title"], meta[property="og:site_name"], meta[property="og:description"], meta[property="og:url"]'
 export function removeTags() {
     const existingMetaTags = document.querySelectorAll(toRemove);
     existingMetaTags.forEach(tag => tag.remove());
