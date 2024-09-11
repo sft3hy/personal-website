70abdf084087b267edfd2655d3171da32cc8196a
Sam Townsend <sft3hy@virginia.edu>
2024-09-11 16:21:49 -0700

    website updates

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,32 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Sitemap from 'vite-plugin-sitemap';
 
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html'
+        }
+        next()
+      })
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
 
   return {
+    appType: 'mpa',
     base: '/sam-townsend/',
     plugins: [
       vue(),
+      rewriteSlashToIndexHtml(),
       Sitemap({
         hostname: 'https://sft3hy.github.io/',
         dynamicRoutes: [
